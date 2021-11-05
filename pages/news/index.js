ad2ba50456661b73b0866a090d36eac0f9429d13
K-Mo2 <karimelsisi27@gmail.com>
2021-11-05 21:37:36 +0200

    Linking between pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
 export default function NewsPage() {
   return (
     <div>
       <h1>This is the News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/param">This is the link to details page</Link>
+        </li>
+        <li>Not a link</li>
+      </ul>
     </div>
   );
 }
