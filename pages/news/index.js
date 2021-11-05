d00e0e163f8061b417963528a172895666fc2174
K-Mo2 <karimelsisi27@gmail.com>
2021-11-05 19:18:57 +0200

    adding nested routes

--- pages/news/index.js
+++ pages/news/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function NewsPage() {
   return (
     <div>
-      <h1>This is the NewsPage</h1>
+      <h1>This is the News Page</h1>
     </div>
   );
 }
